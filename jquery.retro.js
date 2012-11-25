b87b6cba2a6ac149c2dc58979e91a3ef264dd781
zahranisa <samy@jehazy.com>
2012-11-25 11:01:38 +0300

    improvements
    better handling of events in the past.
    stop the timer when it reaches the specified time.
    bug fix

--- jquery.retro.js
+++ jquery.retro.js
@@ -23,7 +23,15 @@
 		// inspired by http://www.javascriptkit.com/script/script2/count2.shtml
 
 		var montharray=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+		var eventDate = new Date(settings['year'],settings['month'],settings['day']);
 		var today=new Date();
+		// if the date is in the past then the counter is stale.
+		var stale = false;
+		if (eventDate < today) {
+			//the requested date is in the past.
+			stale = true;
+		}
+		// getting date components
 		var todayy=today.getYear();
 		if (todayy < 1000)
 			todayy+=1900;
@@ -33,14 +41,35 @@
 		var todaymin=today.getMinutes();
 		var todaysec=today.getSeconds();
 		var todaystring=montharray[todaym]+" "+todayd+", "+todayy+" "+todayh+":"+todaymin+":"+todaysec;
+		var flips_remaining;
 		futurestring=montharray[settings['month']-1]+" "+settings['day']+", "+settings['year'];
+		//dd is difference in milliseconds
 		dd=Date.parse(futurestring)-Date.parse(todaystring);
+
+		// if the difference is negative then it is in the past, the counter should stop and show zeros
+		if (dd < 0) {
+			dd = 0;
+			flips_remaining = 0;
+		}
+		else
+		{
+			if (settings['show_seconds'] == true) {
+				flips_remaining = dd / 1000;
+			}
+			else
+			{
+				flips_remaining = dd / 1000;
+				flips_remaining = flips_remaining / 60;
+			}
+		}
 		var dday=Math.floor(dd/(60*60*1000*24)*1);
 		var dhour=Math.floor((dd%(60*60*1000*24))/(60*60*1000)*1);
 		var dmin=Math.floor(((dd%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);
+		// delta is the difference between the height of the digit based on font size and the hight of the containing div 
 		var delta = (settings['height'] - settings['font-size']) / 2;
 		if(delta < 0)
 			delta = 0;
+
 		var print_digits = function(x,pos){
 			var digits = x+'';
 			digits =  digits.length;
@@ -57,7 +86,6 @@
 				{
 					cur_digit = 0;
 					prev_digit = 0;
-					console.log(cur_digit+" "+prev_digit);
 					single_digit = false;
 				}
 				else
@@ -96,7 +124,7 @@
 
 			},300);
 		};
-		var max_value = {'sec1':9,'sec2':5,'min1':9,'min2':5,'hour1':9,'hour2':5};
+		var max_value = {'sec1':9,'sec2':5,'min1':9,'min2':5,'hour1':9,'hour2':5,'day1':9,'day2':9};
 		if(settings['show_seconds'] == true)
 			var dsec=Math.floor((((dd%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
 
@@ -131,43 +159,50 @@
 			$this.find('.back-bottom, .bottom-half, .shadow > .bottom').css({'border-bottom-left-radius':settings['border-radius']+'px',
 				'border-bottom-right-radius':settings['border-radius']+'px'});
 			var interval = (settings['show_seconds'] == true ? 1000 : 60000);
-
-			setInterval(function(){
-				$this.find('.bottom-half').addClass('ready');
-				$this.find('.digit').each(function()
+			if (!stale)
+			{
+				var clock = window.setInterval(function()
 				{
-					var position = (settings['show_seconds'] == true ? 'sec1':'min1')
-					if($(this).data('cur_pos') == position)
+					$this.find('.bottom-half').addClass('ready');
+					$this.find('.digit').each(function()
 					{
-						var digit = ($(this).data('cur_digit') > 0 ? $(this).data('cur_digit') -1 : max_value[$(this).data('cur_pos')]);
-						var next_digit = (digit > 0 ? digit -1 : max_value[$(this).data('cur_pos')]);;
-						$(this).data('cur_digit',digit);
-						flip_digit($(this),digit,next_digit);
-						if($(this).data('cur_digit') == max_value[$(this).data('cur_pos')])
+						var position = (settings['show_seconds'] == true ? 'sec1':'min1')
+						if($(this).data('cur_pos') == position)
 						{
-							var cur = $(this).prev();
-							while(cur)
+							var digit = ($(this).data('cur_digit') > 0 ? $(this).data('cur_digit') -1 : max_value[$(this).data('cur_pos')]);
+							var next_digit = (digit > 0 ? digit -1 : max_value[$(this).data('cur_pos')]);;
+							$(this).data('cur_digit',digit);
+							flip_digit($(this),digit,next_digit);
+							if(digit == max_value[$(this).data('cur_pos')])
 							{
-								cur_digit = (cur.data('cur_digit'));
-								cur_next_digit = (cur_digit > 0 ? cur_digit -1 : max_value[cur.data('cur_pos')]);
-								cur.data('cur_digit',cur_next_digit);
-								if(cur.data('cur_digit') == max_value[cur.data('cur_pos')])
+								var cur = $(this).prev();
+								while(cur)
 								{
-									cur_next_digit =  cur.data('cur_digit') -1;
-									flip_digit(cur, cur.data('cur_digit'),cur_next_digit);
-									cur = cur.prev();
-								}
-								else
-								{
-									cur_next_digit = ( cur.data('cur_digit') == 0 ? max_value[cur.data('cur_pos')]: cur.data('cur_digit') -1 );
-									flip_digit(cur, cur.data('cur_digit'),cur_next_digit);
-									break;
+									cur_digit = (cur.data('cur_digit'));
+									cur_next_digit = (cur_digit > 0 ? cur_digit -1 : max_value[cur.data('cur_pos')]);
+									cur.data('cur_digit',cur_next_digit);
+									if(cur.data('cur_digit') == max_value[cur.data('cur_pos')])
+									{
+										cur_next_digit =  cur.data('cur_digit') -1;
+										flip_digit(cur, cur.data('cur_digit'),cur_next_digit);
+										cur = cur.prev();
+									}
+									else
+									{
+										cur_next_digit = ( cur.data('cur_digit') == 0 ? max_value[cur.data('cur_pos')]: cur.data('cur_digit') -1 );
+										flip_digit(cur, cur.data('cur_digit'),cur_next_digit);
+										break;
+									}
 								}
 							}
 						}
+					});
+					--flips_remaining;
+					if (flips_remaining <= 0) {
+						window.clearInterval(clock);
 					}
-				});
-			},interval);
+				},interval);
+			}
 		}); 
 	};
 })( jQuery );
